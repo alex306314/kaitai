a604d27f7210519d192b3980648b5af46e543252
yaobo <316841740@qq.com>
2014-11-12 16:33:29 +0800

    add new module

--- sites/all/modules/custom/ktmodule/templates/front_tuijian.tpl.php
+++ sites/all/modules/custom/ktmodule/templates/front_tuijian.tpl.php
@@ -1,8 +1,16 @@
 <div class="c blktjcc clearfix">
   <div class="tjslide myslide">
-    <a href="#">
-      <span class="imgw"><img src="" alt=""></span>
-      <span class="imgt"></span>
+
+    <div class="item">
+      <a href="#" class="a1">
+        <span class="imgw"><img src="" alt=""></span>
+        <span class="imgt"></span>
       </a>
+      <a href="#" class="a2">
+        <span class="imgw"><img src="" alt=""></span>
+        <span class="imgt"></span>
+      </a>
+    </div>
+    
   </div>
 </div>
